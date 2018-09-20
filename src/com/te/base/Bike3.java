459d8fb235aaec12e66726524b3caa548190e2f0
Te Cai <tsai_te@Tes-MacBook-Air.fios-router.home>
2018-09-19 23:10:16 -0400

    re-wrote Bike as Bike 3

--- src/com/te/base/Bike3.java
+++ src/com/te/base/Bike3.java
@@ -3,5 +3,22 @@ public class Bike3 {
     int wheelNum;
     int seatNum;
 
-    public Bike3
+    public Bike3 (){};
+
+    public Bike3 (String ws, int wn, int sn){
+        this.wheelShape=ws;
+        this.seatNum=wn;
+        this.wheelNum=sn;
+    }
+
+    public void print(){
+        System.out.println("ws is:"+wheelShape);
+        System.out.println("wn is:"+wheelNum);
+        System.out.println("sn is"+wheelNum);
+    }
+
+    public static void main (String[] args){
+        Bike3 b3=new Bike3 ("circle", 2,1);
+        b3.print();
+    }
 }
