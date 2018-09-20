b0932c9c087a7f4bdc55fdd086ff31631f204f2f
Te Cai <tsai_te@Tes-MacBook-Air.local>
2018-09-20 18:01:11 -0400

    data type comparsion, == and .equels

--- src/com/te/base/Bike3.java
+++ src/com/te/base/Bike3.java
@@ -20,5 +20,13 @@ public class Bike3 {
     public static void main (String[] args){
         Bike3 b3=new Bike3 ("circle", 2,1);
         b3.print();
+
+        String str1 = new String("1234");
+        String str2 = new String ("1234");
+        if (str1.equals(str2)) System.out.println("string comparsion");
     }
 }
+
+
+
+
