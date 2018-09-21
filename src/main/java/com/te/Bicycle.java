de0cb0c4c681b57ec80ca6aebb761fc7d3afe262
Te Cai <tsai_te@Tes-MacBook-Air.local>
2018-09-21 16:25:35 -0400

    add as a maven project + import(Option+Enter)

--- src/main/java/com/te/Bicycle.java
+++ src/main/java/com/te/Bicycle.java
@@ -1,5 +1,7 @@
 package com.te;
 
+import com.google.common.base.Strings;
+
 public class Bicycle extends Bike {
         int pedalNum;
         public Bicycle (){}
@@ -16,6 +18,9 @@ public class Bicycle extends Bike {
             bi.print();
             String instanceStr = bi.toString();
             System.out.println(instanceStr);
+
+            String st="repeat10";
+            System.out.println(Strings.repeat(st,12));
         }
         int getPedalNum(){
             return pedalNum;
@@ -24,4 +29,6 @@ public class Bicycle extends Bike {
         public String toString(){
             return "wheelShape:" + getWheelShape()+", wheelNum:" + getWheelNum()+", pedalNum:" + getPedalNum();
         }
+
+
 }
